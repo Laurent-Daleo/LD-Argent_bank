03a1ab4fd5ee4b263554e05e3cec7a0917c489a1
Laurent-Daleo <137190234+Laurent-Daleo@users.noreply.github.com>
2024-04-08 18:13:30 +0200

    modification: reusable componant featues on home page

--- argentbank/src/components/features/index.jsx
+++ argentbank/src/components/features/index.jsx
@@ -4,24 +4,28 @@ import chat from './img/icon-chat.png'
 import money from './img/icon-money.png'
 import security from './img/icon-security.png'
 
-const Features = () => {
+const Features = ({ features }) => {
+    const getIcon = (iconName) => {
+        switch (iconName) {
+            case 'chat':
+                return chat;
+            case 'money':
+                return money;
+            case 'security':
+                return security;
+            default:
+                return null;
+        }
+    };
     return (
-        <section className='feature_container' >
-            <div className="feature_item">
-                <img src={ chat} alt="icone chat" className="item_icon" />
-                <h3 className='feature_item_title' >You are our #1 priority</h3>
-                <p>Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.</p>
-            </div>
-            <div className="feature_item">
-                <img src={ money } alt="icone money" className="item_icon" />
-                <h3 className='feature_item_title' >More savings means higher rates</h3>
-                <p>The more you save with us, the higher your interest rate will be!</p>
-            </div>
-            <div className="feature_item">
-                <img src={ security } alt="icone securité" className="item_icon" />
-                <h3 className='feature_item_title' >Security you can trust</h3>
-                <p>We use top of the line encryption to make sure your data and money is always safe.</p>
-            </div>
+        <section className='feature_container'>
+            {features.map(feature => (
+                <div key={feature.id} className="feature_item">
+                    <img src={getIcon(feature.icon)} alt={`icon ${feature.title}`} className="item_icon" />
+                    <h3 className='feature_item_title'>{feature.title}</h3>
+                    <p>{feature.description}</p>
+                </div>
+            ))}
         </section>
     );
 };
--- argentbank/src/pages/home/index.jsx
+++ argentbank/src/pages/home/index.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Header from '../../components/header';
 import Banner from '../../components/banner';
 import Features from '../../components/features';
 import Footer from '../../components/footer';
 
 const Home = () => {
+    const [featureData, setFeatureData] = useState([]);
+
+    useEffect(() => {
+        const fetchFeatureData = async () => {
+            try {
+                const response = await fetch('/features.json');
+                const data = await response.json();
+                setFeatureData(data);
+            } catch (error) {
+                console.error('Error fetching feature data:', error);
+            }
+        };
+
+        fetchFeatureData();
+    }, []);
     return (
         <div className='home_page'>
             <Header />
             <Banner />
-            <Features />
+            <Features features={featureData} />
             <Footer />
         </div>
     );
